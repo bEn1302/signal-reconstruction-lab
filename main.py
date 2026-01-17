13dda8bdb26bcb4696220515fbdd2934f3205228
Ben Emanuel Wießner <benwiessner.dev@gmail.com>
2026-01-17 11:57:22 +0100

    Version 0.4 Deutliche Verbesserung Lagrange-Interp und Code-Verbesserung

--- main.py
+++ main.py
@@ -8,43 +8,52 @@ import os
 # FILE = "AUDIO/aufnahme_1.mp3"
 # FACTOR = 15
 
-while True:
-    input_file = input("Gib den Pfad zu deiner Datei an (z.B: AUDIO/aufnahme_1.mp3): ")
 
-    if os.path.exists(input_file):
-        file_path = input_file
-        break
-    else:
-        print(f"Die Datei '{input_file}' wurde nicht gefunden.")
+def get_user_input():
 
-try:
-    factor = int(
-        input("Jeder wievielte Wert soll genommen werden? (Faktor, z.B. 10): ")
-    )  # 15-20 gut
-    if factor < 1:
-        print("Faktor muss mindestens 1 sein. Setze Faktor auf 1.")
-        factor = 1
-except ValueError:
-    print("Ungültige Eingabe (keine Zahl). Faktor auf Standardwert 15 gesetzt.")
-    factor = 15
+    while True:
+        input_file = input(
+            "Gib den Pfad zu deiner Datei an (z.B: AUDIO/aufnahme_1.mp3): "
+        )
 
-try:
-    show_plots = input("Möchtest du eine grafische Visualisierung? (y/n): ")
-except ValueError:
-    print("Ungültige Eingabe, keine Visualisierung")
+        if os.path.exists(input_file):
+            file_path = input_file
+            break
+        else:
+            print(f"Die Datei '{input_file}' wurde nicht gefunden.")
+
+    try:
+        factor = int(
+            input("Jeder wievielte Wert soll genommen werden? (Faktor, z.B. 10): ")
+        )  # 15-20 gut
+        if factor < 1:
+            print("Faktor muss mindestens 1 sein. Setze Faktor auf 1.")
+            factor = 1
+    except ValueError:
+        print("Ungültige Eingabe (keine Zahl). Faktor auf Standardwert 15 gesetzt.")
+        factor = 15
+
+    show_plots = (
+        input("Möchtest du eine grafische Visualisierung? (y/n): ").lower() == "y"
+    )
+
+    return file_path, factor, show_plots
 
 
 def main():
+    # Eingabe
+    file_path, factor, show_plots = get_user_input()
+
     # Daten aus Datei laden
-    x, sr = audio_io.load_signal(input_file, duration=20)
+    x, sr = audio_io.load_signal(file_path, duration=20)
     t = audio_io.get_time_axis(x, sr)
 
     # Verarbeiten
-    x_samp, t_samp = sip.downsample_signal(x, t, factor)
-    reconstructions = sip.reconstruct_signal(t_samp, x_samp, t)
+    x_samp, t_samp = sip.downsample_signal(x, t, factor)  # Downsampling
+    reconstructions = sip.reconstruct_signal(t_samp, x_samp, t)  # Rekonstruktion
 
     sr_new = sr / factor
-    print(f"--> Das entspricht einer neuen Abtastrate von {round(sr_new,2)} Hz")
+    print(f"--> Das entspricht einer neuen Abtastrate von {round(sr_new,2)} Hz \n")
 
     # Speichern
     audio_io.save_signal("out_stufen.wav", reconstructions["stufen"], sr)
@@ -52,7 +61,8 @@ def main():
     audio_io.save_signal("out_kubisch.wav", reconstructions["kubisch"], sr)
 
     # Darstellen
-    if show_plots == "y":
+    if show_plots:
+        print("--> Generiere Plots...")
         plots.plot_results(t, x, t_samp, x_samp, reconstructions)
         plots.error_analysis(t, x, t_samp, x_samp, reconstructions)
 
--- plots.py
+++ plots.py
@@ -3,22 +3,52 @@ import numpy as np
 from scipy.interpolate import lagrange
 
 """
-Hier werden die Daten zum geplottet
+Hier werden die Daten geplottet
 doc: https://matplotlib.org/stable/users/explain/quick_start.html
 """
 
 
-def plot_results(t_orig, x_orig, t_samp, x_samp, recon_dict, zoom_samples=20):
+# Echte Lagrange Berechnung nur für den Aussschnitt
+def _calculate_local_lagrange(t_samp, x_samp, t_start, n_points, t_poly=None):
+    try:
+        t_lag_samp = t_samp[:n_points]
+        x_lag_samp = x_samp[:n_points]
+        t_lag_local = (
+            t_lag_samp - t_start
+        )  # Zeitverschiebung um t_start (Vermeidung numerischer Probleme)
+
+        poly_lagrange = lagrange(t_lag_local, x_lag_samp)
+
+        if t_poly is None:
+            # vorher : t_poly = np.linspace(t_lag_samp[0], t_lag_samp[-1], 200)
+            t_poly = np.linspace(t_lag_samp[0], t_lag_samp[-1], 200)
+
+        y_poly: np.ndarray = poly_lagrange(t_poly - t_start)
+        return t_poly, y_poly, n_points
+
+    except Exception as e:
+        print(f"Lagrange Fehler: {e}")
+        return None, None, 0
+
+
+def plot_results(
+    t_orig: np.ndarray,
+    x_orig: np.ndarray,
+    t_samp: np.ndarray,
+    x_samp: np.ndarray,
+    recon_dict: dict,
+    zoom_samples=20,
+):
     # --- Plot: Gesamtansicht original Signal ---
-    plt.figure(figsize=(15, 5))
-    plt.plot(t_orig, x_orig, "k", alpha=0.2, label="Original")
-    plt.title("Gesamtübersicht des Originalsignals")
-    plt.xlabel("Zeit [s]")
-    plt.ylabel("Signal x(t)")
-    plt.grid(True)
+    fig1, ax1 = plt.subplots(figsize=(15, 5))
+    ax1.plot(t_orig, x_orig, "k", alpha=0.2, label="Original")
+    ax1.set_title("Gesamtübersicht des Originalsignals")
+    ax1.set_xlabel("Zeit [s]")
+    ax1.set_ylabel("Signal x(t)")
+    ax1.grid(True)
 
     # --- Plot: Zoom-Anischt ---
-    plt.figure(figsize=(15, 8))
+    fig2, ax2 = plt.subplots(figsize=(15, 8))
 
     # Zoom Bereich festlegen
     mid = len(t_samp) // 2  # Mitte der Samplerate
@@ -32,8 +62,8 @@ def plot_results(t_orig, x_orig, t_samp, x_samp, recon_dict, zoom_samples=20):
 
     """ Plots """
     # Original Zeitkontinueirlich und Zeitdiskret
-    plt.plot(t_orig[mask], x_orig[mask], "k", alpha=0.2, lw=3, label="Original x(t)")
-    plt.plot(
+    ax2.plot(t_orig[mask], x_orig[mask], "k", alpha=0.2, lw=3, label="Original x(t)")
+    ax2.plot(
         t_samp[mid : mid + zoom_samples],
         x_samp[mid : mid + zoom_samples],
         "ko",
@@ -41,42 +71,51 @@ def plot_results(t_orig, x_orig, t_samp, x_samp, recon_dict, zoom_samples=20):
     )
 
     # Rekonstruktion
+    """
+    Alt:
     plt.plot(t_orig[mask], recon_dict["stufen"][mask], "r--", label="1) Stufen")
     plt.plot(t_orig[mask], recon_dict["linear"][mask], "g--", label="2) Linear")
     plt.plot(t_orig[mask], recon_dict["kubisch"][mask], "m--", label="3) Kubisch")
+    """
 
-    # Echte Lagrange Berechnung nur für den Aussschnitt
-    try:
-        n_points = 5
-        t_lag_samp = t_samp[mid : mid + n_points]
-        x_lag_samp = x_samp[mid : mid + n_points]
+    # Rekontruktionsmethoden: (Stil,Beschriftung)
+    styles = {
+        "stufen": ("r--", "1) Stufen"),
+        "linear": ("g--", "2) Linear"),
+        "kubisch": ("m--", "3)kubisch"),
+    }
 
-        # Zeitverschiebung um t_start
-        t_lag_local = t_lag_samp - t_start
+    for method, (style, label) in styles.items():
+        if method in recon_dict:
+            ax2.plot(t_orig[mask], recon_dict[method][mask], style, label=label)
 
-        poly_lagrange = lagrange(t_lag_local, x_lag_samp)
-        t_poly = np.linspace(t_lag_samp[0], t_lag_samp[-1], 200)
-        y_poly = poly_lagrange(t_poly - t_start)
+    # Lagrange
+    n_points = int(
+        input("Welche Ordnung soll dein Lagrange-Interpolation haben (1...20)?")
+    )
+    t_poly, y_poly, _ = _calculate_local_lagrange(
+        t_samp[mid:], x_samp[mid:], t_start, n_points
+    )
 
-        plt.plot(
+    if t_poly is not None:
+        ax2.plot(
             t_poly, y_poly, "b:", label=f"4) Lagrange (lokal) {n_points-1}. Ordnung"
         )
 
-    except Exception as e:
-        print(f"Lagrange Fehler: {e}")
-
-    # Skalierung, das y-Achse beim 1,5 fachen des Minimums beginnt/ des Maximums endet
-    plt.ylim(np.min(x_orig[mask]) * 1.5, np.max(x_orig[mask]) * 1.5)
-
-    plt.title("Signalrekonstruktion")
-    plt.xlabel("Zeit [s]")
-    plt.ylabel("Signal x(k)")
-    plt.legend()
-    plt.grid(True, alpha=0.3)
+    y_min, y_max = (
+        np.min(x_orig[mask]),
+        np.max(x_orig[mask]),
+    )  # Skalierung, das y-Achse beim 1,5 fachen des Minimums beginnt/ des Maximums endet
+    ax2.set_ylim(y_min * 1.5, y_max * 1.5)
+    ax2.set_title("Signalrekonstruktion")
+    ax2.set_xlabel("Zeit [s]")
+    ax2.set_ylabel("Signal x(k)")
+    ax2.legend()
+    ax2.grid(True, alpha=0.3)
 
 
 # --- Entwicklung x(f) mit geringerer Abtastung (Faktor = 1...sr)
-def plot_comparison(t_orig, recon_dict, factor):
+def plot_comparison(t_orig: np.ndarray, recon_dict: dict, factor: int):
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(18, 6), sharey=True)
     fig.suptitle(f"Rekonstruktions-Vergleich | Abtastfaktor: {factor}", fontsize=16)
 
@@ -99,7 +138,7 @@ def plot_comparison(t_orig, recon_dict, factor):
 
 
 def error_analysis(t_orig, x_orig, t_samp, x_samp, recon_dict, zoom_samples=20):
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 6), sharey=True)
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(24, 6), sharey=True)
     fig.suptitle(
         "Detaillierte Fehleranalyse (Absoluter Fehler pro Methode)", fontsize=16
     )
@@ -116,13 +155,26 @@ def error_analysis(t_orig, x_orig, t_samp, x_samp, recon_dict, zoom_samples=20):
         "stufen": {"ax": ax1, "color": "r", "label": "Stufen"},
         "linear": {"ax": ax2, "color": "g", "label": "Linear"},
         "kubisch": {"ax": ax3, "color": "m", "label": "Kubisch"},
+        "lagrange": {"ax": ax4, "color": "b", "label": "Lagrange"},
     }
 
     for method, config in setup.items():
-        if method in recon_dict:
+        if method in recon_dict or method == "lagrange":
             ax = config["ax"]
             # Fehlerberechnung nur für den maskierten Bereich
-            abs_error = np.abs(x_orig[mask] - recon_dict[method][mask])
+
+            if method == "lagrange":
+                _, y_poly, order = _calculate_local_lagrange(
+                    t_samp[mid:], x_samp[mid:], t_start, 20, t_poly=t_orig[mask]
+                )
+                abs_error = np.abs(x_orig[mask] - y_poly)
+                if y_poly is not None:
+                    abs_error = np.abs(x_orig[mask] - y_poly)
+                else:
+                    print("Lagrange Berechnung fehlgeschlagen, überspringe Plot.")
+                    continue
+            else:
+                abs_error = np.abs(x_orig[mask] - recon_dict[method][mask])
 
             # Plot auf der jeweiligen Achse (ax.plot statt plt.plot)
             ax.plot(
